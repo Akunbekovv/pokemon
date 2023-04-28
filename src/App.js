50e7b108c6b895bc9c5cc062fa9f39d91249590e
Akunbekovv <akunbekovmaksatt@gmail.com>
2023-04-28 17:45:07 +0600

    poki

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ import AboutPage from "./pages/AboutPage/AboutPage";
 
 function App() {
   return (
-    <>
+    <div className='app'>
       <Routes>
         <Route index element={<MainPage/>}/>
         <Route path='/about/:id' element={<AboutPage/>}/>
       </Routes>
-    </>
+    </div>
   );
 }
 
